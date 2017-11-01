93293b8405f3adcde1b6b14ff38baac54b11ceba
Lưu Đức Trung <luuductrung@itcomma.com>
2017-11-01 23:30:36 +0700

    them startupwindow

---
+++ Project POS/POS/POS/StartupWindows.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace POS
+{
+    /// <summary>
+    /// Interaction logic for StartupWindows.xaml
+    /// </summary>
+    public partial class StartupWindows : Window
+    {
+        public StartupWindows()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(4000);
+                Dispatcher.Invoke(() =>
+                {
+
+                    
+                    Login login = new Login();
+                    login.Show();
+                    this.Close();
+                });
+
+            });
+        }
+    }
+}
