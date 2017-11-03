c07311db71a813e9bfc844c6b4c8acbfee4fc1fa
Nam_Nguyen <nguyenhoangnam@itcomma.com>
2017-11-03 12:18:30 +0700

    Update giao dien thô phần setting food

---
+++ Project POS/POS/POS/EmployeeWorkSpace/SettingFood.xaml.cs
@@ -0,0 +1,35 @@
+﻿using POS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS.EmployeeWorkSpace
+{
+    /// <summary>
+    /// Interaction logic for SettingFood.xaml
+    /// </summary>
+    public partial class SettingFood : Page
+    {
+        public SettingFood()
+        {
+            InitializeComponent();
+            lvData.ItemsSource = ProductData.PList;
+        }
+
+        private void lvData_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
