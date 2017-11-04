e9717de17938d10f503a397f79b81aeb40d1b6f1
Lưu Đức Trung <luuductrung@itcomma.com>
2017-11-04 11:30:53 +0700

    update database (nothing)

---
+++ Project POS/POS/POS.Context/Migrations/201711040428213_update.cs
@@ -0,0 +1,16 @@
+namespace POS.Context
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
