866324eec6ad3605d7b3d72fa7c2bab3ca0882b2
hoangnam3397 <nguyenhoangnam3397@gmail.com>
2017-11-05 16:32:37 +0700

    Update 2 Window Rỗng AdminWindow và WareHouseWindow

---
+++ Project POS/POS/POS/WareHouseWorkSpace/WareHouseWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS.WareHouseWorkSpace
+{
+    /// <summary>
+    /// Interaction logic for WareHouseWindow.xaml
+    /// </summary>
+    public partial class WareHouseWindow : Window
+    {
+        public WareHouseWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void bntLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
