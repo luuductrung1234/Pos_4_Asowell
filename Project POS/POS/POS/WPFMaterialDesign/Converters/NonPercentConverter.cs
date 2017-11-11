9606cd8903895de0735cf3f5c7e90fb13fdf41e5
unknown <kaitodeluxelupin@gmail.com>
2017-11-11 16:24:07 +0700

    up code progress

---
+++ Project POS/POS/POS/WPFMaterialDesign/Converters/NonPercentConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaterialDesignThemes.Wpf.Converters
+{
+    class NonPercentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string val;
+            if (value != null)
+            {
+                val = value as string;
+                return val;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
