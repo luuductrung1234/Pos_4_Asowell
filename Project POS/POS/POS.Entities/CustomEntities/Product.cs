11b433098a925212e284ae7669a351cde7c56000
Lưu Đức Trung <luuductrung@itcomma.com>
2017-11-29 21:07:02 +0700

    update UcMenu, hiển thị ảnh cho món ăn

--- Project POS/POS/POS.Entities/CustomEntities/Product.cs
+++ Project POS/POS/POS.Entities/CustomEntities/Product.cs
@@ -5,6 +5,11 @@ namespace POS.Entities
 {
     public partial class Product
     {
+        public BitmapImage ImageData
+        {
+            get { return LoadImage(ImageLink); }
+        }
+
         public static BitmapImage LoadImage(string filename)
         {
             return new BitmapImage(new Uri(@"/Images/" + filename, UriKind.RelativeOrAbsolute));
